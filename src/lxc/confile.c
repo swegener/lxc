ee7b3157fc98568c167e49b70070a2965f793bd5
Michael Holtz <lxc@my.fqdn.org>
2010-01-08 14:34:13 +0100

    use pivot_root instead of chroot
    lxc currently does a chroot into the target rootfs. chroot is insecure and
    can easily be broken, as demonstrated here:
    
    | root@synergy:~# touch /this_is_the_realrootfs_ouch
    | # touch /container/webhost/this_is_the_container
    | # lxc-start -n webhost /bin/sh
    | # ls this*
    | this_is_the_container
    | # ./breakchroot
    | # ls this*
    | this_is_the_realrootfs_ouch
    
    code to break chroot taken from
    http://www.bpfh.net/simes/computing/chroot-break.html
    
    Now this can be fixed. As our container has his own mount namespace, we can
    easily pivot_root into the rootfs and then unmount all old mounts. The patch
    attached add a new config keyword which contains the path to a temporary
    mount for the old rootfs (inside the container). This stops the chroot break
    method shown before.
    
    Example:
    
    | root@synergy:~# grep pivotdir /var/lib/lxc/webhost/config
    | lxc.pivotdir = /oldrootfs
    | root@synergy:~# ls -lad /container/webhost/oldrootfs
    | drwxr-xr-x 2 root root 4096 2010-01-02 03:59 /container/webhost/oldrootfs
    | root@synergy:~# lxc-start -n webhost /bin/sh
    | # mount -t proc proc /proc
    | # cat /proc/mounts
    | rootfs / rootfs rw 0 0
    | /dev/root / ext3 rw,relatime,errors=remount-ro,data=writeback 0 0
    | devpts /dev/console devpts rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000 0 0
    | proc /proc proc rw,relatime 0 0
    | # ls this*
    | this_is_the_container
    | # ./breakchroot
    | # ls this*
    | this_is_the_container
    
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>
    Signed-off-by: Michael Holtz <lxc@my.fqdn.org>
    (cherry picked from commit bf601689a9e0cea1ceaf17e4f7f853f5392c2827)
    Signed-off-by: Sven Wegener <sven.wegener@stealer.net>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -44,6 +44,7 @@ static int config_tty(const char *, char *, struct lxc_conf *);
 static int config_cgroup(const char *, char *, struct lxc_conf *);
 static int config_mount(const char *, char *, struct lxc_conf *);
 static int config_rootfs(const char *, char *, struct lxc_conf *);
+static int config_pivotdir(const char *, char *, struct lxc_conf *);
 static int config_utsname(const char *, char *, struct lxc_conf *);
 static int config_network_type(const char *, char *, struct lxc_conf *);
 static int config_network_flags(const char *, char *, struct lxc_conf *);
@@ -70,6 +71,7 @@ static struct config config[] = {
 	{ "lxc.rootfs",         config_rootfs         },
 	{ "lxc.utsname",        config_utsname        },
 	{ "lxc.network.type",   config_network_type   },
+	{ "lxc.pivotdir",             config_pivotdir             },
 	{ "lxc.network.flags",  config_network_flags  },
 	{ "lxc.network.link",   config_network_link   },
 	{ "lxc.network.name",   config_network_name   },
@@ -487,6 +489,22 @@ static int config_rootfs(const char *key, char *value, struct lxc_conf *lxc_conf
 
 	lxc_conf->rootfs = strdup(value);
 	if (!lxc_conf->rootfs) {
+		SYSERROR("failed to duplicate string %s", value);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int config_pivotdir(const char *key, char *value, struct lxc_conf *lxc_conf)
+{
+	if (strlen(value) >= MAXPATHLEN) {
+		ERROR("%s path is too long", value);
+		return -1;
+	}
+
+	lxc_conf->pivotdir = strdup(value);
+	if (!lxc_conf->pivotdir) {
 		SYSERROR("failed to duplicate string %s", value);
 		return -1;
 	}
